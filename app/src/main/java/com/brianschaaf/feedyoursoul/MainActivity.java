e613c669f2badd6087925c306c05b368e6f12b7b
Brian Schaaf <brian.schaaf@epicvideo.tech>
2021-06-23 22:06:51 -0400

    added dessert class and styled card same as others cereated new Dish object for desserts and adaped the array to display them ion the ListView

--- app/src/main/java/com/brianschaaf/feedyoursoul/MainActivity.java
+++ app/src/main/java/com/brianschaaf/feedyoursoul/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     CardView startersCard;
     CardView mainCourseCard;
+    TextView emailText;
+    CardView dessertCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainCourseCard = findViewById(R.id.card_view_main);
+        emailText = findViewById(R.id.text_view_email);
+
+
+        emailText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchGmail = new Intent(Intent.ACTION_SENDTO);
+                launchGmail.setData(Uri.parse("mailto:feedYourSoul@reallygood.com"));
+
+                startActivity(launchGmail);
+            }
+        });
 
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +50,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent mainCourseActivityIntent = new Intent(MainActivity.this, MainCourseActivity.class);
 
                 startActivity(mainCourseActivityIntent);
+            }
+        });
+
+        dessertCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dessertActivityIntent = new Intent(MainActivity.this, DessertActivity.class);
+
+                startActivity(dessertActivityIntent);
             }
         });
     }
