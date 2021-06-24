caa4320022d7ab2cbdcf13ca17e4e0157018aed2
Brian Schaaf <brian.schaaf@epicvideo.tech>
2021-06-23 21:05:21 -0400

    added array of staters to StartersActivity class and and array adapter to display all dishes inside the listview

---
+++ app/src/main/java/com/brianschaaf/feedyoursoul/Dish.java
@@ -0,0 +1,4 @@
+package com.brianschaaf.feedyoursoul;
+
+public class Dish {
+}
--- app/src/main/java/com/brianschaaf/feedyoursoul/StartersActivity.java
+++ app/src/main/java/com/brianschaaf/feedyoursoul/StartersActivity.java
@@ -3,6 +3,7 @@ package com.brianschaaf.feedyoursoul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StartersActivity extends AppCompatActivity {
@@ -14,6 +15,29 @@ public class StartersActivity extends AppCompatActivity {
 
         ListView startersList = findViewById(R.id.list_view_starters);
 
-        String[] apps = {"Fried Pickles", "Mozz Sticks", "Spring Rolls", "Wings", "Riblets", }
+        String[] apps = {"Mushroom and tofu maki",
+                "Egg and avocado uramaki",
+                "Melon and lemon soup",
+                "Coconut and chocolate mousse",
+                "Spinach and cabbage wontons",
+                "Broccoli and cucumber soup",
+                "Chilli and aubergine dip",
+                "Chickpea and chilli gyoza",
+                "Sprout and pineapple soup",
+                "Egusi and borscht soup",
+                "Aubergine and egg sushi",
+                "Artichoke and mustard soup",
+                "Peppercorn and tamarind soup",
+                "Parsley and celeriac parcels",
+                "Pasta and broccoli soup",
+                "Potato and courgette soup",
+                "Chickpea and cabbage parcels",
+                "Coriander and peppercorn gyoza",
+                "Pear and chestnut soup",
+                "Pesto and garam masala parcels"};
+
+        ArrayAdapter<String> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, apps);
+
+        startersList.setAdapter(dishesAdapter);
     }
 }
